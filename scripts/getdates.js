fca04a39eea5809d20af1bae1124cd063343e028
florenciataravella <bunibliss@gmail.com>
2025-09-08 10:30:47 -0300

    h

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,17 @@ const year = document.querySelector("#year"); //grabs the element which id=year 
 
 const today = new Date();  // use the date object
 
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`; //I'm creating an element in HTML 
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`; //I'm creating an element in HTML 
+
+const lastmodified=document.querySelector("#lastmodified");
+
+const today2=new Date();
+
+const shortDate  = new Intl.DateTimeFormat("en-US", { dateStyle: "short" }).format(today2); //The Intl.DateTimeFormat constructor lets you format dates according to locale rules.
+
+lastmodified.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat(
+  "en-US",
+  { dateStyle: "short" }
+).format(today)}</span>`;
+
+
