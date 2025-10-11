5c1792aeb96cc75042617ca08fde43e1cb2b78f8
florenciataravella <bunibliss@gmail.com>
2025-10-11 14:10:28 -0300

    g

--- project/scripts/shipping.js
+++ project/scripts/shipping.js
@@ -12,7 +12,9 @@ const shippingChoices = [
          id:"874-j"
     }
 ];
-
+const page = document.body.dataset.page;
+  
+if (page === "shipping") {
 const shipping=document.querySelector("#shippingChoice");
 
 shippingChoices.forEach(sh=>{
@@ -21,8 +23,32 @@ shippingChoices.forEach(sh=>{
   option.textContent=sh.name;
   shipping.appendChild(option);
 })
+}
+if (page === "shipping") {
+const form=document.querySelector(".shippingForm");
+
+let numPurchases=Number(window.localStorage.getItem("numPurchases-ls"))||0;
+
+form.addEventListener("submit", ()=>{
+
+  numPurchases++
+
+  localStorage.setItem("numPurchases-ls", numPurchases);
+
+});
 
 
+if (page=="purchase"){
+
+let numPurchases=Number(window.localStorage.getItem("numPurchases-ls"))||0;
+const purchases=document.createElement("span");
+purchases.textContent=`Number of purchases: ${numPurchases}`;
+const heading=document.querySelector(".thanks h2");
+if (heading) heading.appendChild(purchases);
+console.log(numPurchases);
+
+}
+}
 
 
 
